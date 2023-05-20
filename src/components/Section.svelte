5e571b2b29a465c02017a2b529cf4f72200dc5fa
kurara_ara <kurara.ara@gmail.com>
2023-05-20 18:12:07 +0100

    This is looking bad, but I promise...

---
+++ src/components/Section.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+    // Import components
+    import Stats from './Stats.svelte'
+    import { editing } from '../stores';
+
+    // Export
+    export let section;
+</script>
+
+<div>
+    {#if $editing}
+
+    <h2 bind:innerHTML={section.name} contenteditable="true">{section.name}</h2>
+    {:else}
+    <h2>{section.name}</h2>
+    {/if}
+    <Stats bind:stats={section.stats}/>
+</div>
+
+<style lang="scss">
+    div {
+        padding: 1rem;
+        border-width: 0.15rem;
+        border-style: solid;
+        border-image: 
+        linear-gradient(to bottom, rgba(0, 0, 0, 0), rgb(var(--accent)), rgba(0, 0, 0, 0)) 1 100%;
+        border-right: none;
+    }
+    h2 {
+        text-shadow: var(--shadow);
+        color: rgb(var(--accent));
+        margin-top:0;
+        margin-bottom:0;
+    }
+</style>
